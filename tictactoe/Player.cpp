59f74807123871a90da9f025f69590998a0a614c
Adnan Shaheen <shaheen4qau@gmail.com>
2015-11-01 00:30:13 -0400

    get/set name, symbol

--- tictactoe/Player.cpp
+++ tictactoe/Player.cpp
@@ -1,20 +1,17 @@
+
 #include "Player.h"
 
-
-CPlayer::CPlayer()
+CPlayer::CPlayer() : m_bSymbolXO(true)
 {
 }
-
 
 CPlayer::~CPlayer()
 {
 }
 
-
 CPlayer::CPlayer(const CPlayer& cPlayer)
 {
 }
-
 
 CPlayer& CPlayer::operator = (const CPlayer& cPlayer)
 {
@@ -23,13 +20,11 @@ CPlayer& CPlayer::operator = (const CPlayer& cPlayer)
 	return *this;
 }
 
-
 ostream& CPlayer::operator << (ostream& cOut)
 {
 	//TODO: insert return statement here
 	return cOut;
 }
-
 
 bool CPlayer::operator > (const CPlayer& cPlayer)
 {
@@ -42,8 +37,37 @@ bool CPlayer::operator < (const CPlayer& cPlayer)
 	return false;
 }
 
-
 bool CPlayer::operator == (const CPlayer& cPlayer)
 {
 	return false;
+}
+
+inline string CPlayer::GetPlayerName() const
+{
+	return m_csName;
+}
+
+inline void CPlayer::SetPlayerName(const string csName)
+{
+	m_csName = csName;
+}
+
+inline bool CPlayer::GetSymbol() const
+{
+	return m_bSymbolXO;
+}
+
+inline void CPlayer::SetSymbol(const bool bSymbolXO)
+{
+	m_bSymbolXO = bSymbolXO;
+}
+
+inline bool CPlayer::IsSymbolO() const
+{
+	return m_bSymbolXO == false;
+}
+
+inline bool CPlayer::IsSymbolX() const
+{
+	return m_bSymbolXO == true;
 }
--- tictactoe/Player.h
+++ tictactoe/Player.h
@@ -7,14 +7,30 @@ using namespace std;
 class CPlayer
 {
 public:
-	CPlayer();
-	CPlayer(const CPlayer& cPlayer);
-	~CPlayer();
+	/* constructor/destructor */
+	CPlayer();												// constructor
+	CPlayer(string csName, bool bSymbolXO);					// constructor
+	CPlayer(const CPlayer& cPlayer);						// copy constructor
+	~CPlayer();												// destructor
 
-	CPlayer&	operator = (const CPlayer& cPlayer);
-	ostream&	operator << (ostream& cOut);
-	bool		operator > (const CPlayer& cPlayer);
-	bool		operator < (const CPlayer& cPlayer);
-	bool		operator == (const CPlayer& cPlayer);
+	/* operator overloading */
+	CPlayer&		operator = (const CPlayer& cPlayer);	// assignment operator
+	ostream&		operator << (ostream& cOut);			// extraction operator
+	bool			operator > (const CPlayer& cPlayer);	// greater than operator
+	bool			operator < (const CPlayer& cPlayer);	// less than operator
+	bool			operator == (const CPlayer& cPlayer);	// equal-to operator
+
+	inline string	GetPlayerName() const;					// get player name
+	inline void		SetPlayerName(const string csName);		// set player name
+
+	inline bool		GetSymbol() const;						// get player symbol
+	inline void		SetSymbol(const bool bSymbolXO);		// set player symbol
+
+	inline bool		IsSymbolO() const;						// symbol is O
+	inline bool		IsSymbolX() const;						// symbol is X
+
+private:
+	string m_csName;										// player name
+	bool m_bSymbolXO;										// symbol type (X/O)
 };
 
